5bfebb41200c5602b0e4d8377406cb98c99ed768
deepakajaylinux <amdeepak21@gmail.com>
2015-01-22 01:15:46 +0530

    pharaohtools

--- docs/conf.py
+++ docs/conf.py
@@ -256,3 +256,7 @@ texinfo_documents = [
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
 
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False
+
+
